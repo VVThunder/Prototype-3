fd00829db347e082b6c524aeca64978e23a2d03e
VVThunder <145505541+VVThunder@users.noreply.github.com>
2023-10-31 21:13:48 +0000

    Bonus: Dash Ability & Score Complete

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -17,6 +17,7 @@ public class PlayerController : MonoBehaviour
     public bool gameOver;
     public bool doubleJumpUsed = false;
     public float doubleJumpForce;
+    public bool doubleSpeed = false;
 
     // Start is called before the first frame update
     void Start()
@@ -45,6 +46,17 @@ public class PlayerController : MonoBehaviour
             playerRB.AddForce(Vector3.up * doubleJumpForce, ForceMode.Impulse);
             playerAnim.Play("Running_Jump", 3, 0f);
             playerAudio.PlayOneShot(jumpSound, 1.0f);
+        }
+
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            doubleSpeed = true;
+            playerAnim.SetFloat("Speed_Multiplier", 2.0f);
+        }
+        else if (doubleSpeed)
+        {
+            doubleSpeed = false;
+            playerAnim.SetFloat("Speed_Multiplier", 1.0f);
         }
 
     }
